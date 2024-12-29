dafeaff8fedea641bccd25e4b98ff96a4a9fb988
sujeettx <sujeetkushwaha0123@gmail.com>
2024-12-29 12:51:40 +0530

    ..

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,26 +4,24 @@ dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+
 if (!JWT_SECRET) {
     throw new Error('JWT_SECRET is not defined in environment variables');
 }
 
 export const authenticate = (req, res, next) => {
     try {
-        const authHeader = req.headers.authorization;
-       
-        if (!authHeader) {
+        const token = req.headers.authorization; // Assume token is sent directly in the header
+        
+        if (!token) {
             return res.status(401).json({ message: 'Access denied, no token provided' });
         }
 
-        if (!authHeader.startsWith('Bearer ')) {
-            return res.status(401).json({ message: 'Invalid token format' });
-        }
-
-        const token = authHeader.split(' ')[1];
-        
         try {
+            console.log(token);
+            console.log(JWT_SECRET);
             const decoded = jwt.verify(token, JWT_SECRET);
+
             req.user = decoded;
             next();
         } catch (error) {
