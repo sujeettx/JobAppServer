eb724cb70d0de3634f3e0ca472c20527629c5330
sujeettx <sujeetkushwaha0123@gmail.com>
2024-12-29 21:20:44 +0530

    ...

--- src/routes/jobRoute.js
+++ src/routes/jobRoute.js
@@ -18,18 +18,18 @@ router.use(authenticate);
 // Student routes
 router.get('/',authorize(['student']),getAllJobs);                              // tested
 router.get('/:id',authorize(['student']),getJobById ); // get job by id         // tested
-router.post('/:id/apply', authorize(['student']), applyForJob);
+router.post('/:id/apply', authorize(['student']), applyForJob);                 // tested
 
 // Company routes
 router.post('/', authorize(['company']), createJob);                             // tested
-router.get('/my/:id',getMyJobs);                                                 // tested         
-router.get('/applicants', authorize(['company']), getApplicants);
-router.put('/:id', authorize(['company']), updateJob);
-router.delete('/:id', authorize(['company']), deleteJob);
+router.get('/my/:id',getMyJobs);                                                 // tested    later on for again testing      
+router.get('/applicants', authorize(['company']), getApplicants);                // later test
+router.put('/:id', authorize(['company']), updateJob);                           // tested
+router.delete('/:id', authorize(['company']), deleteJob);                        // test
 
 // Error handler for invalid routes
 router.use('*', (req, res) => {
-    res.status(404).json({ message: 'Job route not found' });
+    res.status(404).json({ message: 'Job route not found please again check the route url' });
 });
 
 export default router;
