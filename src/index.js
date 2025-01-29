5eb853875bc5eb8a6dd59f2840d0457e0785e90f
sujeettx <sujeetkushwaha0123@gmail.com>
2025-01-29 12:22:49 +0530

    add origin local

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ app.use(express.json());
 
 // Enable CORS
 app.use(cors({
-    origin: "https://job-box-tau.vercel.app" || "http://localhost:3000"
-    }
-));
+    origin: ["https://job-box-tau.vercel.app", "http://localhost:3000"]
+}));
+
 // Routes
 app.use('/users', authRoute);
 app.use('/jobs', jobRoute);
