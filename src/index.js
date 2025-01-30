2720cbeeeb1a1f7d5e398d4ea7a2f1e9995ed67b
sujeettx <sujeetkushwaha0123@gmail.com>
2025-01-30 23:23:12 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Enable CORS
 app.use(cors({
-    origin: ["https://jobbox-livid.vercel.app/", "http://localhost:3000"]
+    origin: ["https://jobbox-livid.vercel.app", "http://localhost:3000"]
 }));
 
 // Routes
